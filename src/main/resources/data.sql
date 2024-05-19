237a5c8e414e73775a1f428c29775d594b0522b0
q100vmike <melkin@mail.ru>
2024-05-19 19:44:03 +0300

    commit2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO animal (id, name, breed, cost, character, secretInfo)
+INSERT INTO animal (id, name, breed, cost, "character", secret_Info)
 VALUES(55, 'Cat', 'kitty', 10, 'good', 'secret');
